ebff995fb79e6c0ea9fc1732b48304162dadba66
MatiasRojas24 <113546295+MatiasRojas24@users.noreply.github.com>
2023-08-24 11:09:18 -0300

    Update TP_2_Prog (Matías Rojas).py
    NO TERMINADO

--- TP_2_Prog (Matías Rojas).py
+++ TP_2_Prog (Matías Rojas).py
@@ -29,7 +29,27 @@ print("El menor, de los tres números ingresados, es: ", menor)
 # desde ese año o cuántos años faltan para llegar a ese año.
 years = input("Ingrese el año actual y un año cualquiera [año_actual, año_x]: ")
 an_actual = int(years[0:years.find(",")])
-an_x = int(years.find[" "]+1:)
+an_x = int(years[years.find(" ")+1:])
+if an_actual > an_x:
+    pas = an_actual - an_x
+    print(f"Han pasado {pas} años desde el año {an_x}.")
+if an_actual < an_x:
+    falt = an_x - an_actual 
+    print(f"Faltan {falt} años para el año {an_x}")
+
 # 17-	Requerir al usuario que ingrese un día de la semana e imprimir un mensaje si es lunes, otro mensaje 
 # diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día ingresado no es 
 # ninguno de esos, imprimir otro mensaje.
+dia = input("¿Qué día de la semana es hoy? ")
+dia = dia.lower()
+if dia == "lunes":
+    print(f"Hoy es {dia} ¡feliz inicio de semana!")
+
+if dia == "viernes":
+    print(f"Hoy es {dia} ¡feliz fin de semana!")
+
+if dia == "martes" or dia == "miercoles" or dia == "miércoles" or dia == "jueves":
+    print(f"Hoy es {dia}, a seguir trabajando.")
+
+if dia == "sábado" or dia == "sabado" or dia == "domingo":
+    print(f"Hoy es {dia}, a descansar.")
